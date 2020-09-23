ac27dfe69651949163272a90ad866e789afead76
Noah Shields <noahshields5@gmail.com>
2020-09-23 19:31:41 -0400

    Commented classes include    BasicStageInfo    ChooseStageListActivity

--- app/src/main/java/com/honorsmobileapps/noahshields/smashutilities/StageStrikingFeature/BasicStageInfo.java
+++ app/src/main/java/com/honorsmobileapps/noahshields/smashutilities/StageStrikingFeature/BasicStageInfo.java
@@ -1,9 +1,18 @@
 package com.honorsmobileapps.noahshields.smashutilities.StageStrikingFeature;
 
+//This is a data class that stores info about a stage
+//for the Stage Striking feature
 public class BasicStageInfo {
     private int imageResourceID;
     private String stageName;
     private boolean isSelected;
+
+    //Constructs the Class
+    //Params:
+    //  imageResourceID - the image resource to access for this stage
+    //  stageName - the name of the stage
+    //  isSelected - this variable determines whether this stage has
+    //               been previously selected by the user
     public BasicStageInfo(int imageResourceID, String stageName, boolean isSelected){
         this.imageResourceID = imageResourceID;
         this.stageName = stageName;
--- app/src/main/java/com/honorsmobileapps/noahshields/smashutilities/StageStrikingFeature/ChooseStageListActivity.java
+++ app/src/main/java/com/honorsmobileapps/noahshields/smashutilities/StageStrikingFeature/ChooseStageListActivity.java
@@ -2,7 +2,6 @@ package com.honorsmobileapps.noahshields.smashutilities.StageStrikingFeature;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -15,45 +14,37 @@ import com.honorsmobileapps.noahshields.smashutilities.R;
 import java.util.ArrayList;
 import java.util.Map;
 
+//This activity allows the user to either create a
+//stage list or access one they already stored in
+//shared preferences
 public class ChooseStageListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_stage_list);
-        int[] stageImageResources = {R.drawable.battlefield_image, R.drawable.big_battlefield_image, R.drawable.final_destination_image,
-                R.drawable.peachs_castle_image, R.drawable.kongo_jungle_picture, R.drawable.hyrule_castle_sixty_four_image,
-                R.drawable.super_happy_tree_image, R.drawable.dreamland_image, R.drawable.kongo_falls_image,
-                R.drawable.temple_image, R.drawable.brinstar_image, R.drawable.yoshis_story_image,
-                R.drawable.fountain_of_dreams_image, R.drawable.corneria_image, R.drawable.venom_image,
-                R.drawable.pokemon_stadium_one_image, R.drawable.delfino_plaza_image, R.drawable.wario_ware_image,
-                R.drawable.frigate_orpheon_image, R.drawable.yoshis_island_brawl_image, R.drawable.halberd_image,
-                R.drawable.lylat_image, R.drawable.pokemon_stadium_two_image, R.drawable.castle_siege_image,
-                R.drawable.smashville_image, R.drawable.unova_image, R.drawable.picto_chat_image,
-                R.drawable.kalos_image, R.drawable.town_and_city_image, R.drawable.duck_hunt_image};
-        String[] stageNameAndSize = {"Battle Field", "Big Battle Field", "Final Destination",
-                "Peach's Castle", "Kongo Jungle", "Hyrule Castle",
-                "Super Happy Tree", "Dreamland", "Kongo Falls",
-                "Temple", "Brinstar", "Yoshi's Story",
-                "Fountain of\nDreams", "Corneria", "Venom",
-                "Pokemon Stadium One", "Delfino Plaza", "Wario Ware",
-                "Frigate Orpheon", "Yoshi's Island (Brawl)", "Halberd",
-                "Lylat Cruise", "Pokemon Stadium Two", "Castle Siege",
-                "Smashville", "Unova Pokemon League", "Picto Chat 2",
-                "Kalos Pokemon League", "Town and City", "Duck Hunt"};
+
+        //Access the shared preferences
         SharedPreferences prefs = this.getSharedPreferences("com.honorsmobileapps.noahshields.smashutilities",MODE_PRIVATE);
         Map<String, ?> allPrefs = prefs.getAll();
-        Log.d("Prefs",allPrefs.toString());
+
         ArrayList<StageListInfo> stageListInfos = new ArrayList<>();
+
+        //Loops through all of the entries in the set
         for (Map.Entry<String, ?> entry : allPrefs.entrySet()) {
-            Log.d("Prefs",entry.getKey());
+            //Gets all of the preferences that are saved as a savedstagelist
             if (entry.getKey().contains("com.honorsmobileapps.noahshields.smashutilities.savedstagelist")){
-                stageListInfos.add(new StageListInfo(entry.getKey().substring(62),entry.getKey()));
+                //Parses data into a StageListInfo object
+                stageListInfos.add(new StageListInfo(entry.getKey().substring(62), entry.getKey()));
             }
         }
+
+        //Sets up the RecyclerView for the activity
         RecyclerView recyclerView = findViewById(R.id.RecyclerView_StageListSelection);
         StageListAdapter adapter = new StageListAdapter(stageListInfos);
         recyclerView.setAdapter(adapter);
+
+        //Sets up the button to create a new stage list
         Button button = findViewById(R.id.Button_CreateStageListChoose);
         button.setOnClickListener(new View.OnClickListener() {
             @Override

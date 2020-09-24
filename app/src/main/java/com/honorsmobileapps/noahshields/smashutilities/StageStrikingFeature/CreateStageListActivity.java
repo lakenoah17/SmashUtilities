b21638266f9b88b29fffbe6ebacdbfb32a7aec88
Noah Shields <noahshields5@gmail.com>
2020-09-24 19:43:27 -0400

    Commented classes include    CreateStageListActivity Also fixed functionality of the popup window in the CreateStageListActivity. Previously the window wasn't focusable. This didn't allow for the EditText to be used properly by the user. Fixed the size of the popupwindow by changing it to WrapContent. Submit button still needs to be moved further down in the popup.

--- app/src/main/java/com/honorsmobileapps/noahshields/smashutilities/StageStrikingFeature/CreateStageListActivity.java
+++ app/src/main/java/com/honorsmobileapps/noahshields/smashutilities/StageStrikingFeature/CreateStageListActivity.java
@@ -1,49 +1,39 @@
 package com.honorsmobileapps.noahshields.smashutilities.StageStrikingFeature;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.transition.Explode;
-import android.transition.Transition;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
 
 import com.honorsmobileapps.noahshields.smashutilities.R;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+//This activity allows the user to create a stage list and save it to
+//shared preferences
 public class CreateStageListActivity extends AppCompatActivity {
-    private Button saveStageList;
     private boolean[] selectedStages;
     private static final String SELECTED_STAGES_KEY = "selectedStages";
     final ArrayList<BasicStageInfo> stages = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_stage_list);
-
-        final LinearLayout layout_MainMenu = (LinearLayout) findViewById(R.id.LinearLayout_CreateStageListRootLayout);
 
         //Arrays that store all of the stage names and image resources
         final int[] stageImageResources = {R.drawable.battlefield_image, R.drawable.big_battlefield_image, R.drawable.final_destination_image,
@@ -68,13 +58,14 @@ public class CreateStageListActivity extends AppCompatActivity {
                                     "Kalos Pokemon League", "Town and City", "Duck Hunt"};
 
 
-        //This is for when the screen rotates. Will check if there is a array of stages selected saved
+        //When screen rotates the activity is reset. This loads the selected stages from
+        //the savedInstanceState bundle if there is a saved state to load from
         selectedStages = new boolean[stageNameAndSize.length];
         if (savedInstanceState != null){
             selectedStages = savedInstanceState.getBooleanArray(SELECTED_STAGES_KEY);
         }
 
-        //Makes all of the stage info objects
+        //Makes all necessary stage info objects
         for (int i = 0; i < stageNameAndSize.length; i++){
             stages.add(new BasicStageInfo(stageImageResources[i], stageNameAndSize[i], selectedStages[i]));
         }
@@ -93,26 +84,33 @@ public class CreateStageListActivity extends AppCompatActivity {
         GridLayoutManager gridLayoutManager = (GridLayoutManager)stagesRecycler.getLayoutManager();
         gridLayoutManager.setSpanCount(((int)dpWidth)/200);
 
-
         stagesRecycler.setAdapter(adapter);
 
         //Setting up save stage list button
-        saveStageList = findViewById(R.id.Button_SaveStageList);
+        Button saveStageList = findViewById(R.id.Button_SaveStageList);
         saveStageList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 //Makes popup for the save stage list
                 View customView = getLayoutInflater().inflate(R.layout.confirm_stage_list_creation_popup, null);
-                final PopupWindow popupWindow = new PopupWindow(customView, (int)(dpWidth / 2), (int)(dpHeight / 2));
+                final PopupWindow popupWindow = new PopupWindow(customView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 popupWindow.setElevation(20);
                 popupWindow.setEnterTransition(new Explode());
+
+                //Allows for window to be focused
+                popupWindow.setFocusable(true);
 
                 //Retrieving all of the components of the popup for access
                 ImageButton closePopup = customView.findViewById(R.id.ImageButton_Close);
                 Button submit = customView.findViewById(R.id.Button_CreateStageList);
+
                 final EditText submitName = customView.findViewById(R.id.EditText_NameStageList);
+                submitName.setFocusable(true);
+                popupWindow.update();
+
                 popupWindow.showAtLocation((ViewGroup)v.getParent(), Gravity.CENTER,0,0);
+
 
                 //For when the user submits the stage list they created
                 submit.setOnClickListener(new View.OnClickListener() {
@@ -128,14 +126,14 @@ public class CreateStageListActivity extends AppCompatActivity {
                         }
 
                         //Saving the stage list to Shared Preferences
-                        SharedPreferences sharedPreferences = getSharedPreferences("com.honorsmobileapps.noahshields.smashutilities",Context.MODE_PRIVATE);
+                        SharedPreferences sharedPreferences = getSharedPreferences("com.honorsmobileapps.noahshields.smashutilities", Context.MODE_PRIVATE);
                         SharedPreferences.Editor editor = sharedPreferences.edit();
-                        String stageListName = "com.honorsmobileapps.noahshields.smashutilities.savedstagelist"+submitName.getText().toString();
+                        String stageListName = "com.honorsmobileapps.noahshields.smashutilities.savedstagelist" + submitName.getText().toString();
                         editor.putStringSet(stageListName, set);
-                        editor.commit();
+                        editor.apply();
 
-//                        Intent returnToStageSelections = new Intent(v.getContext(), ChooseStageListActivity.class);
-//                        startActivity(returnToStageSelections);
+                       //Intent returnToStageSelections = new Intent(v.getContext(), ChooseStageListActivity.class);
+                       //startActivity(returnToStageSelections);
 
                         finish();
                     }

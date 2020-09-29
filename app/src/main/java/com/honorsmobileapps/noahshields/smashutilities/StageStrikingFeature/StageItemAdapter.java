45ac534a838a448286a179cb33d04ba7722778fc
Noah Shields <noahshields5@gmail.com>
2020-09-28 20:02:58 -0400

    Commented classes include    StageItemAdapter    StageListAdapter

--- app/src/main/java/com/honorsmobileapps/noahshields/smashutilities/StageStrikingFeature/StageItemAdapter.java
+++ app/src/main/java/com/honorsmobileapps/noahshields/smashutilities/StageStrikingFeature/StageItemAdapter.java
@@ -12,9 +12,12 @@ import android.widget.TextView;
 import com.honorsmobileapps.noahshields.smashutilities.R;
 import java.util.ArrayList;
 
-
+//Works as the adapter for the RecyclerViews in the
+//CreateStageListActivity and the StageStrikingActivity
 public class StageItemAdapter extends RecyclerView.Adapter<StageItemAdapter.StageItemViewHolder> {
     private ArrayList<BasicStageInfo> stages;
+
+    //The ItemViewHolder for stage items
     public static class StageItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private ImageView stageImage;
         private TextView stageName;
@@ -24,6 +27,11 @@ public class StageItemAdapter extends RecyclerView.Adapter<StageItemAdapter.Stag
         private int imageResourceID;
         private String stageNameString;
         private ImageView strikeImage;
+
+        //Constructs the ViewHolder
+        //Params:
+        //  itemView - the item of the RecyclerView that this
+        //          ViewHolder will contain
         public StageItemViewHolder(View itemView){
             super(itemView);
             context = itemView.getContext();
@@ -31,9 +39,15 @@ public class StageItemAdapter extends RecyclerView.Adapter<StageItemAdapter.Stag
             stageImage = itemView.findViewById(R.id.ImageView_StageImage);
             stageName = itemView.findViewById(R.id.TextView_StageName);
             strikeImage = itemView.findViewById(R.id.ImageView_StageIsStriked);
+
             itemView.setOnClickListener(this);
 
         }
+
+        //Binds the appropriate information to the itemView
+        //Params:
+        //  basicStageInfo - data class containing all of the
+        //           necessary info to populate the itemView
         public void bindStageItem(BasicStageInfo basicStageInfo){
             this.basicStageInfo = basicStageInfo;
             this.imageResourceID = basicStageInfo.getImageResourceID();
@@ -42,9 +56,16 @@ public class StageItemAdapter extends RecyclerView.Adapter<StageItemAdapter.Stag
             stageImage.setImageResource(imageResourceID);
             stageName.setText(stageNameString);
 
+            //When the activity is restarted by a phone orientation
+            //change or something else this makes sure the itemView
+            //is properly updated to it's previous state
             if (basicStageInfo.getSelected()){
                 background.setBackgroundTintList(ColorStateList.valueOf(itemView.getResources().getColor(R.color.selected)));
                 background.setCardElevation(12f);
+
+                //The StageStrikingActivity has an extra image laid
+                //over every item that's selected.
+                //The image is a big red X
                 if (context instanceof StageStrikingActivity)
                     strikeImage.setVisibility(View.VISIBLE);
             }
@@ -53,41 +74,86 @@ public class StageItemAdapter extends RecyclerView.Adapter<StageItemAdapter.Stag
                 background.setCardElevation(2f);
             }
         }
+
+        //When an item is clicked it changes its appearance
         @Override
         public void onClick(View v) {
+            //Checks if the item is already selected or not and unselects it
             if (basicStageInfo.getSelected()){
+                //Changes background color
                 background.setBackgroundTintList(ColorStateList.valueOf(v.getResources().getColor(R.color.unselected)));
+
+                //Changes the visibility of the image
+                // special to StageStrikingActivity
                 if (v.getContext() instanceof StageStrikingActivity)
                     strikeImage.setVisibility(View.GONE);
+
                 background.setCardElevation(12f);
             }
             else{
+                //Changes background color
                 background.setBackgroundTintList(ColorStateList.valueOf(v.getResources().getColor(R.color.selected)));
+
+                //Changes the visibility of the image
+                // special to StageStrikingActivity
                 if (v.getContext() instanceof StageStrikingActivity)
                     strikeImage.setVisibility(View.VISIBLE);
                 background.setCardElevation(2f);
             }
+
+            //Changes the selected value to its opposite.
             basicStageInfo.setSelected(!basicStageInfo.getSelected());
         }
     }
+
+    //Constructs the adapter
+    //Params:
+    //  stages - an ArrayList containing all of the stages
+    //              to be used in the RecyclerView
     public StageItemAdapter(ArrayList<BasicStageInfo> stages){
         this.stages = stages;
     }
+
+    //Gets the size of the RecyclerView
+    //Params: N/A
+    //Returns: the size of the RecyclerView
     @Override
     public int getItemCount() {
         return stages.size();
     }
+
+    //Inflates the view to use for an item in the RecyclerView
+    //Params:
+    //  viewGroup - used to get the context of the RecyclerViews
+    //              Activity and as the group this view is a part
+    //              of
+    //  i - the index of the item in the RecyclerView
+    //Returns: a new ViewHolder for the adapter
     @Override
     public StageItemAdapter.StageItemViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        //Gets inflater
         LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+        //Inflates a new item
         View view = layoutInflater.inflate(R.layout.stage_item, viewGroup, false);
+        //Returns the new ViewHolder
         return new StageItemAdapter.StageItemViewHolder(view);
     }
+
+    //Starts the binding process for the ViewHolder
+    //Params:
+    //  holder - the ViewHolder to bind
+    //  position - the position of the ViewHolder in the RecyclerView
     @Override
     public void onBindViewHolder(StageItemAdapter.StageItemViewHolder holder, int position) {
         BasicStageInfo stageInfo = stages.get(position);
         holder.bindStageItem(stageInfo);
     }
+
+    //This method allows the user to change the list the user
+    //uses for the RecyclerView
+    //Currently Unused
+    //Params:
+    //  stages - the ArrayList to replace the current one with
     public void setStages(ArrayList<BasicStageInfo> stages){
         this.stages = stages;
     }
--- app/src/main/java/com/honorsmobileapps/noahshields/smashutilities/StageStrikingFeature/StageListAdapter.java
+++ app/src/main/java/com/honorsmobileapps/noahshields/smashutilities/StageStrikingFeature/StageListAdapter.java
@@ -9,49 +9,98 @@ import android.widget.TextView;
 import com.honorsmobileapps.noahshields.smashutilities.R;
 import java.util.ArrayList;
 
-
+//Works as the adapter for the RecyclerView in the
+//ChooseStageListActivity
 public class StageListAdapter extends RecyclerView.Adapter<StageListAdapter.StageListViewHolder> {
-        private ArrayList<StageListInfo> stageLists;
-        public static class StageListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-            private TextView name;
-            private StageListInfo stageListInfo;
+    private ArrayList<StageListInfo> stageLists;
 
-            public StageListViewHolder(View itemView){
-                super(itemView);
-                name = itemView.findViewById(R.id.TextView_StageListName);
-                itemView.setOnClickListener(this);
-            }
-            public void bindStageItem(StageListInfo stageListInfo){
-                this.stageListInfo = stageListInfo;
-                name.setText(stageListInfo.getName());
-            }
-            @Override
-            public void onClick(View v) {
-                Intent startNext = new Intent(v.getContext(), StageStrikingActivity.class);
-                startNext.putExtra("SharedPrefKey",stageListInfo.getKey());
-                startNext.putExtra("StageListName",stageListInfo.getName());
-                v.getContext().startActivity(startNext);
-            }
+    //ViewHolder class for the items of the RecyclerView
+    public static class StageListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView name;
+        private StageListInfo stageListInfo;
+
+        //Constructs the ViewHolder
+        //Params:
+        //  itemView - the item contained by this ViewHolder
+        public StageListViewHolder(View itemView){
+            super(itemView);
+            name = itemView.findViewById(R.id.TextView_StageListName);
+            itemView.setOnClickListener(this);
         }
-        public StageListAdapter(ArrayList<StageListInfo> stageLists){
-            this.stageLists = stageLists;
+
+        //Binds the appropriate values of the itemView
+        //Params:
+        //  stageListInfo - class with all of the data
+        //                  necessary to populate the itemView
+        public void bindStageItem(StageListInfo stageListInfo){
+            this.stageListInfo = stageListInfo;
+            name.setText(stageListInfo.getName());
         }
+
+        //When this item is clicked it starts the StageStrikingActivity
+        //Params:
+        //  v - the view that was clicked
         @Override
-        public int getItemCount() {
+        public void onClick(View v) {
+            //Creates Intent to start next Activity and adds the necessary
+            //data for the next activity as extras.
+            Intent startNext = new Intent(v.getContext(), StageStrikingActivity.class);
+            startNext.putExtra("SharedPrefKey", stageListInfo.getKey());
+            startNext.putExtra("StageListName", stageListInfo.getName());
+
+            v.getContext().startActivity(startNext);
+        }
+    }
+
+    //Constructs the StageListAdapter
+    //Params:
+    //  stageLists - list containing all of the
+    //              StageListInfos needed for the RecyclerView
+    public StageListAdapter(ArrayList<StageListInfo> stageLists){
+        this.stageLists = stageLists;
+    }
+
+    //Gets the size of the RecyclerView
+    //Params: N/A
+    //Returns: the size of the RecyclerView
+    @Override
+    public int getItemCount() {
             return stageLists.size();
         }
-        @Override
-        public StageListAdapter.StageListViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-            LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
-            View view = layoutInflater.inflate(R.layout.stage_list_item, viewGroup, false);
-            return new StageListAdapter.StageListViewHolder(view);
-        }
-        @Override
-        public void onBindViewHolder(StageListAdapter.StageListViewHolder holder, int position) {
-            StageListInfo stageListInfo = stageLists.get(position);
-            holder.bindStageItem(stageListInfo);
-        }
-        public void setStages(ArrayList<StageListInfo> stageListInfos){
-            this.stageLists = stageListInfos;
-        }
+
+    //Inflates the view to use for an item in the RecyclerView
+    //Params:
+    //  viewGroup - used to get the context of the RecyclerViews
+    //              Activity and as the group this view is a part
+    //              of
+    //  i - the index of the item in the RecyclerView
+    //Returns: a new ViewHolder for the adapter
+    @Override
+    public StageListAdapter.StageListViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        //Gets the LayoutInflater to inflate the view
+        LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+        //Inflates the view
+        View view = layoutInflater.inflate(R.layout.stage_list_item, viewGroup, false);
+        //Returns the new ViewHolder
+        return new StageListAdapter.StageListViewHolder(view);
+    }
+
+    //Starts the binding process for the ViewHolder
+    //Params:
+    //  holder - the ViewHolder to bind
+    //  position - the position of the ViewHolder in the RecyclerView
+    @Override
+    public void onBindViewHolder(StageListAdapter.StageListViewHolder holder, int position) {
+        StageListInfo stageListInfo = stageLists.get(position);
+        holder.bindStageItem(stageListInfo);
+    }
+
+    //This method allows the user to change the list the user
+    //uses for the RecyclerView
+    //Currently Unused
+    //Params:
+    //  stageListInfos - the ArrayList to replace the current one with
+    public void setStages(ArrayList<StageListInfo> stageListInfos){
+        this.stageLists = stageListInfos;
+    }
 }
